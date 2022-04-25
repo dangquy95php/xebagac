a96508bac6e1a3cfe30c133861c60880290cfa94
pxwebshop@gmail.com <pxwebshop@gmail.com>
2022-04-25 22:11:56 +0700

    ADD

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,21 +1,13 @@
 $(document).ready(function() {
     $(".c-slider").slick({
-        infinite: false,
+        infinite: true,
       
-        responsive: [{
-      
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              infinite: true
-            }
-      
-          }, {
-      
+        responsive: [
+          {
             breakpoint: 600,
             settings: {
-              slidesToShow: 2,
-              dots: true
+              slidesToShow: 1,
+              dots: true,
             }
       
           }, {
@@ -25,19 +17,29 @@ $(document).ready(function() {
       
           }]
     });
+
+    $(".c-list1__item").click(function(e) {
+        $(".c-list1__item").each(function() {
+          $(this).removeClass("is-active");
+        })
+        e.preventDefault();
+        var aid = $(this).children().attr("class");
+        $('html,body').animate({scrollTop: $("#"+ aid).offset().top - 100},'slow');
+    });
+    
     $(".c-header__icon").click(function() {
         if ($(this).hasClass("is-active")) {
-            $(this).children().css("display", "none");
-            $(this).children().next().css("display", "block");
+            // $(this).children().css("display", "none");
             $(this).removeClass("is-active");
-            $('.c-list1').addClass("is-show");
-            $(".c-list1").slideToggle(200);
-
-        } else {
-            $(this).children().css("display", "block");
-            $(this).children().next().css("display", "none");
-            $(this).addClass("is-active");
+            $("body").css("overflow", "");
             $('.c-list1').removeClass("is-show");
+
+            $(".c-list1").slideToggle(200);
+        } else {
+            // $(this).children().next().css("display", "block");
+            $(this).addClass("is-active");
+            $("body").css("overflow", "hidden");
+            $('.c-list1').addClass("is-show");
             $(".c-list1").slideToggle(200);
         }
     });
